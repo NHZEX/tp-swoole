b9f8381252cae4ed33633bdd4f798785dcdc8849
auooru <auooru@163.com>
2019-10-14 13:52:17 +0800

    fix route 内存泄漏

--- src/Tp/Http.php
+++ src/Tp/Http.php
@@ -3,6 +3,36 @@ declare(strict_types=1);
 
 namespace HZEX\TpSwoole\Tp;
 
+use think\Route;
+
 class Http extends \think\Http
 {
+    /** @var Route */
+    protected static $route;
+
+    protected function loadRoutes(): void
+    {
+        if (!isset(self::$route)) {
+            parent::loadRoutes();
+            self::$route = clone $this->app->route;
+        }
+    }
+
+    protected function dispatchToRoute($request)
+    {
+        if (isset(self::$route)) {
+            $newRoute = clone self::$route;
+            $app      = $this->app;
+            $closure  = function () use ($app) {
+                $this->app = $app;
+            };
+
+            $resetRouter = $closure->bindTo($newRoute, $newRoute);
+            $resetRouter();
+
+            $this->app->instance("route", $newRoute);
+        }
+
+        return parent::dispatchToRoute($request);
+    }
 }
