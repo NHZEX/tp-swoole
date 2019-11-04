02b7af947d99126c1b5e5801070432ba0c78bd81
auooru <auooru@163.com>
2019-11-04 09:24:46 +0800

    移除弃用函数

--- src/helpers.php
+++ src/helpers.php
@@ -5,19 +5,6 @@ use HZEX\TpSwoole\Service;
 define('HZEX_SWOOLE_ENABLE', extension_loaded('swoole') && 'cli' === PHP_SAPI);
 
 /**
- * 是否存在 swoole
- * @return bool
- */
-function exist_swoole(): bool
-{
-    static $exist;
-    if (null === $exist) {
-        $exist = extension_loaded('swoole');
-    }
-    return $exist;
-}
-
-/**
  * 获取内存统计
  */
 function stats_memory()
